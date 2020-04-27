e99237ec47e6c0e33c255d0582926b6e0d4e4f50
aoki <a058265@gmail.com>
2020-04-27 17:19:34 +0900

    add axios

--- src/states/modules/apiTestModule.ts
+++ src/states/modules/apiTestModule.ts
@@ -1,5 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const get = (keyword: string) => {
+  axios
+    .get(
+      `http://ws.audioscrobbler.com/2.0/?method=album.search&album=${keyword}&limit=12&api_key=${api_key}&format=json`,
+      {
+        responseType: "json"
+      }
+    )
+    .then(function(res) {
+      const result: Albummatches = qs.parse(res.data.results.albummatches);
+      const albms: Array<Album> = result.album;
+      console.log(albms);
+      albms.forEach(item => console.log(item.artist));
+      setRes(albms);
+    })
+    .catch(function(error) {
+      console.log("ERRRRRRRROR", error);
+      return [];
+    });
+};
+
 type State = {
   url: string;
 };
